adc118dc526f188aadc37fd72564ff55de91b9a4
Noyan Alimov <noyan.alimov@gmail.com>
2020-02-16 19:54:21 +0000

    failed to secure api, please help

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex');
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,6 +15,18 @@ var textapi = new AYLIENTextAPI({
   application_id: 'a8a8d48b',
   application_key: '1249e3696d2279ea9f379f346429f99b'
 });
+
+app.post('/text', (req, res) => {
+    textapi.entities({
+    text: req.body.input
+  }, function(error, response) {
+    if (error === null) {
+      res.json(response);
+    } else {
+      res.status(400).json('error working with api')
+    }
+  });
+})
 
 app.listen(3001, () => {
   console.log('app is running on port 3001');
